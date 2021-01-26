54e9650d13d54e84c5d6e494f0c0c19c6b21a86d
Michalis <michalis.iona@edu.turkuamk.fi>
2021-01-26 13:22:58 +0200

    Finalized Lab 1 to Lab 4

---
+++ Basics_of_Programming/Lab1/arrangenumbers_0_1.py
@@ -0,0 +1,9 @@
+"""
+Lab 1 - arrangenumbers_0_1.py
+Description - Update 1: - 8-puzzle arrangement game with an inversion counter and a randomized number order. 
+@author - Michalis Iona
+
+"""
+print('8 puzzle - arrange the numbers by swapping them with the empty place')
+n= (input('give a number: '))
+print('the number is', n)
